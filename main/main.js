6ca4d0d6ffa7c592713605d683faae78969c24d9
eugene0928 <umid09tuxtayev@gmail.com>
2022-02-07 15:50:17 +0500

    functions

---
+++ main/main.js
@@ -0,0 +1,34 @@
+const search = document.querySelector("#search")
+const read = document.querySelector("#read")
+const header = document.querySelector("#heder")
+const p = document.querySelector("#num")
+const res = document.querySelector("#res")
+
+
+search.onkeyup = async (event) => {
+    if(event.keyCode == 13 && +search.value == search.value && +search.value >= 1 && +search.value <= 114) {
+        let data = await fetch(`https://api.quran.sutanlab.id/surah/${search.value}`)
+        let sura = await data.json()
+
+       
+        header.textContent = `Sura: ${sura.data.name.transliteration.en}`
+        p.textContent = sura.data.verses.length + " ayat"
+
+
+        for(let i of sura.data.verses) {
+            const li = makeLi()
+            console.log(i);
+            li.textContent = i.text.arab
+            res.append(li)
+        }
+
+        search.value = null
+    }
+
+}
+
+
+
+function makeLi() {
+    return document.createElement("li")
+}
